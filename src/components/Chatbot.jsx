a8376a3b5435a31b00c2d3d730833bd419f841ec
Tom <to.olufemi1@gmail.com>
2025-07-18 21:29:52 -0600

    chore: July 18th: forget to commit 2 days ago that I did parseInput.js and successfully parsed user input.

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -2,8 +2,11 @@ import MessageList from "./MessageList.jsx";
 import InputBox from "./InputBox.jsx";
 import PropertyResults from "./PropertyResults.jsx";
 import "./Chatbot.css";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { fetchListings } from "../api/realEstateAPI.js";
+import {parseInput} from "../../utils/parseInput.js";
+import {fetchMessages} from "../api/OpenAI.js";
+
 
 function Chatbot() {
   const [messages, setMessages] = useState([
@@ -11,13 +14,30 @@ function Chatbot() {
     { id: 2, sender: "bot", text: "Sure! Do you have a price range?" }
     ]);
     
-    function onSendMessage() {
-      setMessages(...prevMessages, { role: 'user', content: 'input'})
-    }
     const [properties, setProperties] = useState([]);
-   async function handleUserMessage(input) {
-      const listings = await fetchListings();
-      setProperties(listings);
+
+    async function handleUserMessage(input) {
+      // Update User Messages 
+     const newUserMessage = {
+      id: messages.length + 1, sender: 'user', text: input
+     };
+     const newMessages = [...messages, newUserMessage];
+     // Get ChatGPT's response
+     const response = await fetchMessages(newMessages);
+     const botResponse = {
+      id: newMessages.length + 1, sender: 'bot', text: response
+     };
+     const updatedMessages = [...newMessages, botResponse]
+     setMessages(updatedMessages);
+    // Parse User Input
+      const result = parseInput(input);
+      const allListings = await fetchListings();
+      const filtered = allListings.filter(listing => {
+        if (listing.city === result.location) {
+          return true;
+        }
+      })
+      setProperties(filtered);
     }
     return (
       <div>
