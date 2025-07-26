c52a61f1e90ccee1e5bf174979ca70b9cf812887
Tom <to.olufemi1@gmail.com>
2025-07-25 22:06:42 -0600

    July 25th: Maps are now showing on my app, I will integrate it into app tomorrow

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
 import Chatbot from "./components/Chatbot.jsx";
-
-
+import Maps from './components/Map.jsx';
+import {APIProvider} from '@vis.gl/react-google-maps';
 function App() {
+  const apiKey = import.meta.env.VITE_APP_GOOGLEMAPS_API_KEY
   return (
-    <div className="app">
-       <Chatbot/>
-    </div>
+    <div className="app" style={{ height: "100vh", width: "100vw" }}>
+      <APIProvider apiKey={apiKey}>
+        <Maps/>
+       </APIProvider>
+        {/* <Chatbot/> */}
+   </div>
   );
 }
 
--- src/api/googleMaps.js
+++ src/api/googleMaps.js
@@ -1,10 +1,8 @@
-// import {APIProvider, Map, MapCameraChangedEvent} from '@vis.gl/react-google-maps';
-// <APIProvider apiKey={'Your API key here'} onLoad={() => console.log('Maps API has loaded.')}>
-//      <Map
-//       defaultZoom={13}
-//       defaultCenter={ { lat: -33.860664, lng: 151.208138 } }
-//       onCameraChanged={ (ev: MapCameraChangedEvent) =>
-//         console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
-//       }>
-// </Map>
-// </APIProvider>
+
+function loadMaps() {
+    
+}
+
+function geoAdresss(params) {
+    
+}
