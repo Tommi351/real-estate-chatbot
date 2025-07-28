2024b1e20101ebc495d45188a87666293beed080
Tom <to.olufemi1@gmail.com>
2025-07-27 22:26:06 -0600

    July 27th: I have added the lat/long for listings, will start adding Markers tomorrow, tried to do it now but failed. Will do that tomorrow and hopefully move on to backend. Final stretch.

--- src/api/googleMaps.js
+++ src/api/googleMaps.js
@@ -1,8 +1,14 @@
+const fs = require('fs');
 
-function loadMaps() {
+async function geoCodeCities() {
+ fs.readFile("public/data/listings.json", "utf-8", (err, data) => {
+   let rawData = JSON.parse(data);
+   const locations = rawData.map(c => c.location)
+   
+   for(let city of cities) {
     
+   }
+   
+ });
 }
-
-function geoAdresss(params) {
-    
-}
+module.exports(geoCodeCities);
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,11 +3,12 @@ import {Map} from '@vis.gl/react-google-maps';
 function Maps() {
     return (
     <Map
-      defaultZoom={13}
+      defaultZoom={15}
       defaultCenter={ { lat: -33.860664, lng: 151.208138 } }
       onCameraChanged={ (ev) =>
         console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
       }
+      mapId={import.meta.env.VITE_APP_MAPSID_API_KEY}
       />
     );
 }
