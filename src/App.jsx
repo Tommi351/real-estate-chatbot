b1924c77c56c4deb9c4a48c4dcc3db1fc1beca19
Tom <to.olufemi1@gmail.com>
2025-07-27 22:26:06 -0600

    July 27th: I have added the lat/long for listings, will start adding Markers tomorrow, tried to do it now but failed. Will do that tomorrow and hopefully move on to backend. Final stretch.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
 import Chatbot from "./components/Chatbot.jsx";
 import Maps from './components/Map.jsx';
-import {APIProvider} from '@vis.gl/react-google-maps';
+import {APIProvider, AdvancedMarker, Pin} from '@vis.gl/react-google-maps';
 function App() {
   const apiKey = import.meta.env.VITE_APP_GOOGLEMAPS_API_KEY
   return (
-    <div className="app" style={{ height: "100vh", width: "100vw" }}>
+    <div className="app" style={{ height: "350px", width: "350px" }}>
       <APIProvider apiKey={apiKey}>
         <Maps/>
        </APIProvider>
