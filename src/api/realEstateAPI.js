25a90530cdd1cd69854ece5205fd7b7794bfe222
Tom <to.olufemi1@gmail.com>
2025-08-05 22:41:17 -0600

    feat: Has started backend work, will commit work on backend branch

--- src/api/realEstateAPI.js
+++ src/api/realEstateAPI.js
@@ -1,9 +1,7 @@
-import axios from 'axios';
-
 export async function fetchListings() {
     try {
-    let listings = await axios.get('/public/data/listings.json');
-    return listings.data;
+  const res = await fetch("http://localhost:5000/api/listings");
+  const listings = await res.json();
     } catch (err) {
      throw new Error("Couldn't fetch the listings");
     }
