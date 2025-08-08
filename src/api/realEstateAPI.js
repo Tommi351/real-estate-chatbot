e39f7e5b1779ab448cdedeae90ec9cb073a1b05d
Tom <to.olufemi1@gmail.com>
2025-08-07 22:16:10 -0600

    Going to commit on backend branch. Just need to do this so files I made don't go away

--- src/api/realEstateAPI.js
+++ src/api/realEstateAPI.js
@@ -1,8 +1,10 @@
+import axios from "axios";
 export async function fetchListings() {
     try {
-  const res = await fetch("http://localhost:5000/api/listings");
-  const listings = await res.json();
+  const res = await axios.get("http://localhost:5000/api/listings");
+  const listings = await res.data;
+  return listings;
     } catch (err) {
-     throw new Error("Couldn't fetch the listings");
+     throw new Error("Couldn't fetch the listings, Please fix", err);
     }
 }
