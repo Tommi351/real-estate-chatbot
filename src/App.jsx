32dff31b21b0b674844e11f227faf93ac255b6f3
Tom <to.olufemi1@gmail.com>
2025-08-12 18:21:30 -0600

    feat: have completed the backend(server, routes, controllers) and did the frontend to backend connection. Ready to move to Phase 5: Polish, Testing and Deployment

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,11 @@ import './App.css'
 import Chatbot from "./components/Chatbot.jsx";
 import Maps from './components/Map.jsx';
 import {APIProvider} from '@vis.gl/react-google-maps';
-// import {convertToPois} from '../utils/convertToPois.js';
 
-// const pois = convertToPois();
 function App() {
-  const apiKey = import.meta.env.VITE_APP_GOOGLEMAPS_API_KEY
   return (
     <div className="app">
       <Chatbot/>
-      {/* <APIProvider apiKey={apiKey}>
-        <div style={{ height: "350px", width: "400px" }}>
-        <Maps/>
-        </div>
-       </APIProvider> */}
    </div>
   );
 }
