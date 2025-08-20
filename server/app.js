80b31df187d54b11a85df257ef684d5abdb63fa1
Tom <to.olufemi1@gmail.com>
2025-08-19 20:37:11 -0600

    feat: Project is done, added the index.html and got everything ready for deployment

--- server/app.js
+++ server/app.js
@@ -4,24 +4,25 @@ const cors = require('cors');
 const listingsRoutes = require("./routes/listings");
 const gptRoutes = require("./routes/gpt");
 const app = express();
-
+const port = process.env.PORT || 5000;
 app.use(cors({
   origin: 'http://localhost:5173', // 👈 allow your frontend to talk to backend
 }));
 app.use(express.json());
-
+// Test route
 app.get("/api/test", (req, res) => {
     res.send("Server is working")
 });
 
+// Routes
 app.use("/api/listings", listingsRoutes);
-
 app.use("/api/gpt", gptRoutes);
 
+// Connecting Maps to frontend
 app.get("/maps-key", (req, res) => {
   res.json({key: process.env.GOOGLEMAPS_API_KEY})
 })
 
-app.listen(5000, () => {
+app.listen(port, () => {
     console.log("Serving on port 5000!")
 });
